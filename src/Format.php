5be4e794734ef7d3eff1c21799f81911ad28c331
buzak <buzak@trexima.sk>
2020-02-07 10:27:27 +0100

    Add format expressions for school and kov codes

--- src/Format.php
+++ src/Format.php
@@ -11,5 +11,11 @@ namespace Trexima\HarveyClient;
 final class Format
 {
     public const KOV_SCHOOL_FORMAT = "%2s.%9s.%7s",
+        KOV_FORMAT = "%4s%1s%2s";
+
+    public const SCHOOL_REGEX = '/^[0-9]{9}$/',
+        KOV_REGEX = '/^[0-9a-zA-Z]{7}$/',
+        KODFAK_REGEX = '/^[0-9]{4}$/',
+        EDUID_REGEX = '/^[0-9]{9}$/',
         KOV_SCHOOL_REGEX = '/^[0-9]{2}\.[0-9a-zA-Z]{9}\.[0-9a-zA-Z]{7}$/';
 }
