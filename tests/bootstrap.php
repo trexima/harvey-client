940912f78614d6245ea5e4b0a91f642ab1439ec4
Peter Culka <culka@trexima.sk>
2021-03-05 16:54:13 +0100

    try to fix Travis CI .env parsing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,3 +4,8 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $env = new \Symfony\Component\Dotenv\Dotenv();
 $env->load(__DIR__.'/../.env');
+var_dump($_ENV);
+var_dump(getenv('HARVEY_URL'));
+if (!array_key_exists('HARVEY_URL', $_ENV)) {
+    $_ENV['HARVEY_URL'] = getenv('HARVEY_URL');
+}
